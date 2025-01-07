0f7cc5594d5c198a60c39d0224f86af29126460a
Anand Shekhar <66034323+anandshekhr@users.noreply.github.com>
2025-01-07 09:35:58 +0530

    UPDATE marketing admin

--- src/marketing/admin.py
+++ src/marketing/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+@admin.register(WhatsappChat)
+class WhatsappChatAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(WhatsappDeliveryStatus)
+class WhatsappDeliveryStatusAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(WhatsAppInboundMessage)
+class WhatsAppInboundMessageAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(WhatsAppOutboundMessage)
+class WhatsAppOutboundMessageAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(WhatsappSeenReport)
+class WhatsappSeenReportAdmin(admin.ModelAdmin):
+    pass
+    
+
+
+    
+
+    
+
+    
