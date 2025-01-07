49ab5f885eb9087e0ef0058abb4eb162ec53d4e8
Anand Shekhar <66034323+anandshekhr@users.noreply.github.com>
2025-01-07 07:29:36 +0530

    ADD mysql

--- src/onlinetentstore/settings.py
+++ src/onlinetentstore/settings.py
@@ -90,12 +90,23 @@ WSGI_APPLICATION = 'onlinetentstore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',  
+            'NAME': 'vams-ots',         
+            'USER': 'ashekhar',          
+            'PASSWORD': '1@v@msots',  
+            'HOST': 'ashekhar.mysql.pythonanywhere-services.com',                   
+            # 'PORT': os.environ.get('DB_PORT'),                       
+        }
     }
-}
 
 
 # Password validation
