a484d47befe1f31d6e1a93703d04b5e74093dea1
Anand Shekhar <66034323+anandshekhr@users.noreply.github.com>
2025-01-26 21:41:45 +0530

    UPDATE remove spectacular

--- src/onlinetentstore/urls.py
+++ src/onlinetentstore/urls.py
@@ -12,9 +12,9 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 
 urlpatterns = [
     # Schema endpoints
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    # path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    # path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
     # Other endpoints
     path('admin/', admin.site.urls),
