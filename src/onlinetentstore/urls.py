b61f4f7f118e9a1fe4daeb8529658a8f52aebc7c
Anand Shekhar <66034323+anandshekhr@users.noreply.github.com>
2025-01-26 21:38:43 +0530

    UPDATE api urls

--- src/onlinetentstore/urls.py
+++ src/onlinetentstore/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for onlinetentstore project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path,include, re_path, register_converter
 from .utils import HashIdConverter, FloatConverter
@@ -24,36 +8,25 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 ...
-
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Online APIs",
-      default_version='v1',
-      description="API service for multiple applications",
-      terms_of_service="https://www.vamsbookstore.in/terms-of-service/",
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
-
-
-admin.site.site_header = "Admin"
-admin.site.index_title = "Admin"
-admin.site.site_title = "Admin"
-
-register_converter(HashIdConverter, "hashid")
-register_converter(FloatConverter, "float")
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
-    path('', schema_view.with_ui('swagger', cache_timeout=0)),
+    # Schema endpoints
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
+    # Other endpoints
     path('admin/', admin.site.urls),
-    path('account/',include('accounts.urls')),
-    path('product/',include('products.urls')),
-    path('wishlist/',include('cart.urls')),
-    path('order/',include('orders.urls')),
-    path('payment/',include('payments.urls')),
-    path('supplier/',include('supplier.urls')),
-    path('marketing/',include('marketing.urls')),
+    path('api/v1/account/', include('accounts.urls')),
+    path('api/v1/product/', include('products.urls')),
+    path('api/v1/wishlist/', include('cart.urls')),
+    path('api/v1/order/', include('orders.urls')),
+    path('api/v1/payment/', include('payments.urls')),
+    path('api/v1/supplier/', include('supplier.urls')),
+    path('api/v1/marketing/', include('marketing.urls')),
+    path('api/v1/service/',include('service.urls')),
 ]
+
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
